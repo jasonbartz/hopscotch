e5c0e51a947cd1891a0947a7defdfc06bfc12470
Jason Bartz <jrbartz@gmail.com>
2011-12-31 15:57:52 -0500

    changed models to be more generic

--- hopscotch/apps/dram/models.py
+++ hopscotch/apps/dram/models.py
@@ -3,28 +3,23 @@ from django.db import models
 
 # Local Django Libraries
 from hopscotch.apps.base import BaseModel
-from hopscotch.apps.geo.models import Location
 
 # Location and Meta Information
-class Distributor(BaseModel):
-    '''
-    Represents the whiskey distributor
-    '''
-    
-    location = models.ForeignKey(Location)
-    
-class Distillery(BaseModel):
-    '''
-    Represents a place where whiskey is distilled
-        Keep in mind, this may not be the same as the distributor
-    '''
-    
-    location = models.ForeignKey(Location)
-    distributor = models.ForeignKey(Distributor)
-    
 
+class PlaceType(BaseModel):
+    '''
+    Type of place that manufactures drink
+        Distillery, Brewery
+    '''
+    place_type = models.CharField(max_length=255)
+    
+class Place(BaseModel):
+    '''
+    Represents a place that makes 
+    '''
+    distributor = models.ForeignKey(PlaceType)
 
-# Whiskey information
+# Information
 
 RATING = (
     ('1','Don\'t try',),
@@ -33,30 +28,22 @@ RATING = (
     ('4','Delicious',),
     ('5','Must have',),
 )
-class WhiskeyType(BaseModel):
+class DrinkType(BaseModel):
     '''
-    Is it a real Kentucky Bourbon, or something from the Islay
-        coast?  Use this model to build canonical types of whisk(e)y
+    Type of drink: beer, whiskey
     '''
-    pass
+    drink_type = models.CharField(max_length=255)
     
-class Enjoying(models.Model):
+class Drink(BaseModel):
     '''
-    An experimental model that represents that state of something being enjoyed
-    '''
-    currently_being_enjoyed = models.BooleanField(default=False)
-    neat = models.BooleanField(default=True) #True whiskey default
-    
-class Whiskey(BaseModel):
-    '''
-    Represents a single whiskey.
+    Represents a single drink.
     '''
     ## Whiskey Information
     # The canonical name of the whiskey
     #   Will be shown on the page
     full_name = models.CharField(max_length=255)
-    distillery = models.ForeignKey(Distillery)
-    whiskey_type = models.ForeignKey(WhiskeyType)
+    place = models.ForeignKey(Place)
+    drink_type = models.ForeignKey(DrinkType)
     # Actual bottled year, i.e., 1996
     year = models.IntegerField()
     # Exact Date whiskey released to the public
@@ -73,5 +60,5 @@ class Whiskey(BaseModel):
     # state of drinking
     #! NOTE: Do we need to set some sort of expiration
     #! ISSUE #1
-    enjoying = models.ForeignKey(Enjoying, null=True)
+    enjoying = models.BooleanField()
     own = models.BooleanField()
