31ec1fc1652ff74d3c26e4a15575a96f3c286879
Jason Bartz <jrbartz@gmail.com>
2012-02-19 10:32:47 -0500

    more changes

--- hopscotch/dram/documents.py
+++ hopscotch/dram/documents.py
@@ -5,9 +5,6 @@ from mongoalchemy.document import Document
 from mongoalchemy import fields
 
 # Local lib
-from hopscotch.dram.db import Connect
-
-connection = Connect()
 
 
 RATING = (
@@ -22,8 +19,8 @@ class BaseDocument(Document):
     '''
     A base class that all Dram classes will inherit from
     '''
-    created = fields.DateTimeField()
-    modified = fields.DateTimeField()
+    created = fields.DateTimeField(required=False)
+    modified = fields.DateTimeField(required=False)
 
 class Drink(BaseDocument):
     '''
@@ -33,27 +30,27 @@ class Drink(BaseDocument):
     # The canonical name of the drink
     #   Will be shown on the page
     name = fields.StringField()
-    maker = fields.StringField()
+    maker = fields.StringField(required=False)
     
     # Distillery, brewery, etc
-    maker_type = fields.StringField()
-    drink_type = fields.StringField()
+    maker_type = fields.StringField(required=False)
+    drink_type = fields.StringField(required=False)
     
     # Actual bottled year, i.e., 1996
-    age = fields.IntField()
+    age = fields.IntField(required=False)
     # Exact Date drink released to the public
-    release_date: datetime.datetime,
+    release_date = fields.DateTimeField(required=False)
     # Manufacturer's description, often listed on the bottle
-    manu_desc = fields.StringField()
+    manu_desc = fields.StringField(required=False)
     
     ## Personal/rating information
     # Personal description, from experience
-    personal_desc = fields.StringField()
-    rating = fields.IntField()
+    personal_desc = fields.StringField(required=False)
+    rating = fields.IntField(required=False)
 
     # state of drinking
-    enjoying = fields.BoolField()
-    own = fields.BoolField()
+    enjoying = fields.BoolField(required=False)
+    own = fields.BoolField(required=False)
     # Information representing the location queried
     #   against the Foursquare API
-    location = fields.DictField(),
+    # location = fields.DictField()
