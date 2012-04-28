19746aa5a187a0b4cd438daa340e850ded39c815
Jason Bartz <jrbartz@gmail.com>
2012-04-28 12:08:07 -0400

    adding views/urls/more js libs

--- hopscotch/dram/urls.py
+++ hopscotch/dram/urls.py
@@ -4,12 +4,19 @@ urls.py
 """
 
 from django.conf.urls import patterns, url, include
+from django.shortcuts import render_to_response
 from tastypie.api import Api
-from hopscotch.dram.api import DrinkResource
 
+from hopscotch.dram.api import DrinkResource
+from hopscotch.dram.views import Public
+
+
+# API URLs
 v1_api = Api(api_name='v1')
 v1_api.register(DrinkResource())
 
+
 urlpatterns = patterns('',
+    (r'^$', Public.as_view()),
     (r'^api/', include(v1_api.urls)),
 )
--- hopscotch/dram/views.py
+++ hopscotch/dram/views.py
@@ -1,46 +1,37 @@
-# Third Party Libraries
-from flask import render_template
-from flask.views import View
+import json
+
+from django.views.generic import TemplateView
+from django.db import settings
 from mongoengine import connect
-# Local
+
 from hopscotch.dram.documents import Drink, User
 
-connect('hopscotch')
 
-class BaseView(View):
-    methods         = ['GET']
+class BaseView(TemplateView):
 
-class Home(BaseView):
-    def dispatch_request(self, username=None):
-        return render_template('home.html')
-
-class Checkin(BaseView):
-    def dispatch_request(self, username=None):
-        return render_template('checkin.html')
-
-class Cellar(BaseView):
-    '''
-    Show the drinks in the collection
-    '''
-    def dispatch_request(self, username=None):
+    def _get_config(self):
+        """
+        Get the config json, currently located in hopscotch.dram
+        """
+        with open('%s/hopscotch/dram/config.json' % settings.PROJECT_ROOT,'rb') as config_file:
+            self.config = json.loads(config_file.read())
+    
+    def get_context_data(self, **kwargs):
         
-        context = {
-            'user': [],
-            'drinks': []
-        }
-        
-        user = User.objects.get(username=username)
-        context['user'] = user
-        
-        for drink in user.drinks:
-            context['drinks'].append(Drink.objects.get(drink_id=drink))
-            
-            
-        return render_template('cellar.html', **context)
+        self._get_config()
 
-class PublicFeed(BaseView):
-    """
-    Show drinks recently bought or checked-in to.
-    """
-    def dispatch_request(self):
-        pass
+        return({
+            'js': self.config['js'],
+            # 'css': self.config['css'],
+            'static_url': self.config['static_url']
+        })
+
+
+class Public(BaseView):
+    template_name = 'public.html'
+
+class UserHome(BaseView):
+    template_name = 'user/home.html'
+
+class UserCellar(BaseView):
+    template_name = 'user/cellar.html'
